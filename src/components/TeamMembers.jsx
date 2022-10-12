4f3696ed7e5edd6c0ebaf487309303e0ddf1810d
Erudite885 <erudite885@gmail.com>
2022-10-12 02:50:02 +0100

    props name fixed

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {TeamMemberCard} from "./index";
 
 
-const TeamsMembers = ({employees, selectedTeam, handleEmployeeCardClick}) => {
-  return employees.map((employee) => (
-    <TeamMemberCard
-      employee={employee}
-      selectedTeam={selectedTeam}
-      handleEmployeeCardClick={handleEmployeeCardClick}
-    />
-  ));
-};
-
+const TeamsMembers = ({employees, selectedTeam, handleCardClick}) => {
+    return employees.map((employee) => (
+        <TeamMemberCard
+        employee={employee}
+        selectedTeam={selectedTeam}
+        handleCardClick={handleCardClick}
+        />
+        ));
+    };
+    
 export default TeamsMembers;
