cd1030962ce8bcf8db3585c597aebb84ea4b4a31
Erudite885 <erudite885@gmail.com>
2022-10-12 02:46:21 +0100

    GTM: team members collapse fixed, handleTeamCliick function e defined, gTM function spacing fixed

--- src/components/GroupedTeamMembers.jsx
+++ src/components/GroupedTeamMembers.jsx
@@ -2,23 +2,22 @@ import React, {useState} from "react";
 
 const GroupedTeamMembers = ({ employees, selectedTeam, setSelectedTeam }) => {
 
-    
     const groupTeamMembers = () => {
         let teams = [];
         let teamAMembers = employees.filter((employee) => employee.teamName === 'TeamA');
-        let teamA = {team:'TeamA', members:teamAMembers, collapsed: selectedTeam === 'TeamA'?false:true}
+        let teamA = {team:'TeamA', members:teamAMembers, collapsed: selectedTeam === 'TeamA' ? false : true}
         teams.push(teamA);
         
         let teamBMembers = employees.filter((employee) => employee.teamName === 'TeamB');
-        let teamB = {team:'TeamB', members:teamBMembers, collapsed: selectedTeam === 'TeamB'?false:true}
+        let teamB = {team:'TeamB', members:teamBMembers, collapsed: selectedTeam === 'TeamB' ?false : true}
         teams.push(teamB);
         
         let teamCMembers = employees.filter((employee) => employee.teamName === 'TeamC');
-        let teamC = {team:'TeamC', members:teamCMembers, collapsed: selectedTeam === 'TeamC'?false:true}
+        let teamC = {team:'TeamC', members:teamCMembers, collapsed: selectedTeam === 'TeamC'? false : true}
         teams.push(teamC);
         
         let teamDMembers = employees.filter((employee) => employee.teamName === 'TeamD');
-        let teamD = {team:'TeamD', members:teamDMembers, collapsed: selectedTeam === 'TeamD'?false:true}
+        let teamD = {team:'TeamD', members:teamDMembers, collapsed: selectedTeam === 'TeamD'? false : true}
         teams.push(teamD);
         
         return teams;
@@ -26,7 +25,7 @@ const GroupedTeamMembers = ({ employees, selectedTeam, setSelectedTeam }) => {
 
     const [groupedEmployees, setGroupedData] = useState(groupTeamMembers());
     
-    const handleTeamClick = () => {
+    const handleTeamClick = (e) => {
         let transformedGroupData = groupedEmployees.map((groupedData) => groupedData.team === e.currentTarget.id ? {...groupedData,collapsed:!groupedData.collapsed} : groupedData);
         setGroupedData(transformedGroupData);
         setSelectedTeam(e.currentTarget.id);
@@ -38,7 +37,11 @@ const GroupedTeamMembers = ({ employees, selectedTeam, setSelectedTeam }) => {
       {
         groupedEmployees.map((item) => {
             return (
-              <div key={item.team} className="mt-3 cursor-pointer">
+              <div
+                key={item.team}
+                id={item.team}
+                className="mt-3 cursor-pointer"
+              >
                 <hr className="border-1 border-sky-600 w-[240px] mx-auto" />
                 <h4
                   id={item.team}
@@ -50,7 +53,7 @@ const GroupedTeamMembers = ({ employees, selectedTeam, setSelectedTeam }) => {
                 <div
                   id={"collapse_" + item.team}
                   className={`${
-                    item.collapsed === true ? "collapse" : ""
+                    item.collapsed === true ? "hidden" : ""
                   } mb-4 text-[16px]`}
                 >
                   <hr className="border-1 border-sky-600 w-[240px] mx-auto" />
