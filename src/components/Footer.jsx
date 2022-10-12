40d97596b110b2e1c4cdd593bf3c92cc91569017
Erudite885 <erudite885@gmail.com>
2022-10-12 02:42:57 +0100

    Footer: text size adjusted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   let today = new Date();
 
 
-  return <footer className="text-center font-semibold">
+  return <footer className="text-center text-[14px] font-semibold mt-8">
     <h5>Team Member Allocation App - {today.getFullYear()} </h5>
   </footer>;
 }
